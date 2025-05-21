3d6bc648f4898b8604d577b9af154c93f148b991
Pedro Henrick <pedro.henrickcs@gmail.com>
2025-05-20 21:24:57 -0300

    feat: 🎸 create menubar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,18 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+import { Menubar, MenubarMenu, MenubarTrigger, MenubarContent, MenubarItem, MenubarSeparator } from '@radix-ui/react-menubar';
 import Image from 'next/image';
+import { MenubarShortcut } from './ui/menubar';
 
 export default function Header() {
+    const [openMenu, setOpenMenu] = useState<string | null>("produtos")
+
+    useEffect(() => {
+        console.log('openMenu', openMenu);
+
+    }, [])
+
     return (
         <header className="border-b shadow-sm">
             <div className="flex items-center justify-between px-4 py-3 bg-white">
@@ -23,13 +35,70 @@ export default function Header() {
                 </div>
             </div>
 
-            <nav className="text-white text-sm bg-primary">
+            {/* <nav className="text-white text-sm bg-[#E98C3D]">
                 <ul className="flex justify-center space-x-8 py-2">
-                    <li className="hover:underline cursor-pointer">Início</li>
-                    <li className="hover:underline cursor-pointer">Sobre</li>
-                    <li className="hover:underline cursor-pointer">Produtos</li>
+                    <li className="cursor-pointer rounded p-2 hover:transition-colors hover:text-[#E98C3D] hover:bg-[#ffffff]">Início</li>
+                    <li className="cursor-pointer rounded p-2 hover:transition-colors hover:text-[#E98C3D] hover:bg-[#ffffff]">Sobre</li>
+                    <li className="cursor-pointer rounded p-2 hover:transition-colors hover:text-[#E98C3D] hover:bg-[#ffffff]">Produtos</li>
                 </ul>
-            </nav>
+            </nav> */}
+
+            <Menubar className="flex justify-center bg-[#E98C3D] text-white text-sm py-2 space-x-8 rounded-none border-none">
+                <MenubarMenu>
+                    <MenubarTrigger className="cursor-pointer rounded p-2 hover:transition-colors hover:text-[#E98C3D] hover:bg-[#ffffff] hover:border-0">
+                        Início
+                    </MenubarTrigger>
+                </MenubarMenu>
+
+                <MenubarMenu>
+                    <MenubarTrigger className="cursor-pointer rounded p-2 hover:transition-colors hover:text-[#E98C3D] hover:bg-[#ffffff] hover:border-0">
+                        Sobre
+                    </MenubarTrigger>
+                </MenubarMenu>
+
+                <MenubarMenu>
+                    <div
+                        className="relative"
+                        onMouseEnter={() => {
+                            if (openMenu !== "produtos") {
+                                setOpenMenu("produtos")
+
+                            }
+                            console.log('openMenu', openMenu);
+                        }}
+                        onMouseLeave={() => {
+                            console.log('openMenusdsd', openMenu);
+
+                            setOpenMenu('produtos')
+                        }}
+                    >
+                        <MenubarTrigger
+                            className="hover:underline cursor-pointer bg-transparent m-2 text-white"
+                            onClick={(e) => e.preventDefault()}
+                        >
+                            Produtos
+                        </MenubarTrigger>
+
+                        {openMenu === "produtos" && (
+                            <MenubarContent
+                                align="center"
+                                className="absolute left-1/2 -translate-x-1/2 mt-2 bg-[#E98C3D] text-white rounded shadow"
+                            >
+                                <MenubarItem className="cursor-pointer rounded p-2 hover:transition-colors hover:text-[#E98C3D] hover:bg-[#ffffff]">
+                                    Bebidas
+                                </MenubarItem>
+                                <MenubarItem className="cursor-pointer rounded p-2 hover:transition-colors hover:text-[#E98C3D] hover:bg-[#ffffff]">
+                                    Congelados
+                                </MenubarItem>
+                                <MenubarItem className="cursor-pointer rounded p-2 hover:transition-colors hover:text-[#E98C3D] hover:bg-[#ffffff]">
+                                    Tabacaria
+                                </MenubarItem>
+                            </MenubarContent>
+                        )}
+                    </div>
+                </MenubarMenu>
+            </Menubar>
+
         </header>
     );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,11 +11,9 @@ export default function Hero() {
             alignItems: 'end'
         }}>
             <div className="bg-amber-700 bg-opacity-20 p-6 rounded-md inline-block">
-                <h1 className="text-3xl font-bold mb-2">Do campo para sua cozinha</h1>
-                <p className="text-lg mb-4">Produtos frescos direto do produtor</p>
-                {/* <button className="bg-green-600 text-white px-4 py-2 rounded">Ver Produtos</button> */}
+                <h1 className="text-white text-3xl font-bold mb-2">Do campo para sua cozinha</h1>
+                <p className="text-white text-lg mb-4">Produtos frescos direto do produtor</p>
                 <Button>Ver produtos</Button>
-
             </div>
         </section >
     );
